07b15ab06301ec2912db3a5fde4b467ab412fccb
aakarsh.yadav-at-585310183519 <aakarsh.yadav@qualitics.us>
2018-11-15 14:24:32 +0530

    Adding temp api requrs in apiRequest fiel

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const API_ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast?q=";
+const API_ROOT_URL = "http://localhost:3000/";
 const API_KEY = "&appid=828a7f789fa0d9c17339483615f573da";
 
-export async function get(pincode) {
-  return await axios.get(`${API_ROOT_URL}${pincode}${API_KEY}`);
+export async function get(url) {
+  return await axios.get(`${API_ROOT_URL}${url}`);
+}
+export async function post(url, body) {
+  return await axios.post(`${API_ROOT_URL}${url}`, { ...body });
 }
